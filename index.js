fb4b363d45bdb3598f841828c1957639befcb51a
dylanw422 <71285357+dylanw422@users.noreply.github.com>
2022-12-28 09:27:04 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -21,6 +21,25 @@ let validCVV = false;
 
 let validForm = false;
 
+//LUHN ALGORITHM
+function luhn_checksum(code) {
+    var len = code.length
+    var parity = len % 2
+    var sum = 0
+    for (var i = len-1; i >= 0; i--) {
+        var d = parseInt(code.charAt(i))
+        if (i % 2 == parity) { d *= 2 }
+        if (d > 9) { d -= 9 }
+        sum += d
+    }
+    return sum % 10
+}
+
+//LUHN VALIDATE
+function luhn_validate(fullcode) {
+    return luhn_checksum(fullcode) == 0
+}
+
 function fadeAnimation() {
     logo.classList.remove('animated')
     
@@ -78,25 +97,25 @@ fccn.addEventListener('input', () => {              //CREDIT CARD NUMBER
     }
 
     //card type validation
-    if (fccn.value.match(/3[47][0-9]{13}/)) {
+    if (fccn.value.match(/3[47][0-9]{13}/) && luhn_validate(fccn.value)) {
         logo.src = 'images/amex.png'
         fadeAnimation()
         fccn.style.border = '2px solid rgb(85, 211, 131)'
         console.log('AMEX')
         validCCN = true;
-    } else if (fccn.value.match(/65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})/)) {
+    } else if (fccn.value.match(/65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})/) && luhn_validate(fccn.value)) {
         logo.src = 'images/discover.png'
         fadeAnimation()
         fccn.style.border = '2px solid rgb(85, 211, 131)'
         console.log('Discover')
         validCCN = true;
-    } else if (fccn.value.match(/(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))/)) {
+    } else if (fccn.value.match(/(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))/) && luhn_validate(fccn.value)) {
         logo.src = 'images/master.png'
         fadeAnimation()
         fccn.style.border = '2px solid rgb(85, 211, 131)'
         console.log('MasterCard')
         validCCN = true;
-    } else if (fccn.value.match(/4[0-9]{15}?/)) {
+    } else if (fccn.value.match(/4[0-9]{15}?/) && luhn_validate(fccn.value)) {
         logo.src = 'images/visa.png'
         fadeAnimation()
         fccn.style.border = '2px solid rgb(85, 211, 131)'
